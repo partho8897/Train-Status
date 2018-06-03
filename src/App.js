2ab5bada7bf7dfa35a5e1146f533dedc8688d12f
Partho Paul <partho8897@gmail.com>
2018-06-03 15:23:34 +0530

    Title changed

--- src/App.js
+++ src/App.js
@@ -1,110 +1,188 @@
 import React, { Component } from "react";
+import "./style.css";
 
 export default class App extends Component {
-    state = {
-      sourceStation:"ghy",
-      destinationStation:"ndls",
-      date:"03-06-2018",
-      submit:"",
-      trainData:[],
-      selectedTrain:{
-        classes:[]
+  state = {
+    sourceStation: "jp",
+    destinationStation: "kota",
+    date: "11-06-2018",
+    trainData: [],
+    trainAvailabilty: {},
+    selectedTrain: {
+      classes: []
+    }
+  };
+
+  onSourceStationChange = e => {
+    this.setState({
+      sourceStation: e.currentTarget.value
+    });
+  };
+
+  onDestinationStationChange = e => {
+    this.setState({
+      destinationStation: e.currentTarget.value
+    });
+  };
+
+  onDateChange = e => {
+    this.setState({
+      date: e.currentTarget.value
+    });
+  };
+
+  submit = e => {
+    console.log(this.state);
+    fetch(
+      `//api.railwayapi.com/v2/between/source/${
+        this.state.sourceStation
+      }/dest/${this.state.destinationStation}/date/${
+        this.state.date
+      }/apikey/tsvbv1syei/`
+    )
+      .then(a => a.json())
+      .then(result => {
+        console.log(result);
+        this.setState({
+          trainData: result.trains || []
+        });
+      });
+  };
+
+  selectTrain = train => {
+    this.setState({
+      selectedTrain: train
+    });
+
+    train.classes.forEach(tClass => {
+      fetch(
+        `https://api.railwayapi.com/v2/check-seat/train/${
+          train.number
+        }/source/${train.from_station.code}/dest/${
+          train.to_station.code
+        }/date/${this.state.date}/pref/${
+          tClass.code
+        }/quota/GN/apikey/tsvbv1syei/`
+      )
+        .then(a => a.json())
+        .then(result => {
+          const temp = this.state.trainAvailabilty;
+          temp[tClass.code] = result.availability;
+          this.setState({
+            trainAvailabilty: temp
+          });
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    });
+  };
+
+  render() {
+    var availabilityDates = [];
+
+    Object.keys(this.state.trainAvailabilty).forEach(key => {
+      if (this.state.trainAvailabilty[key].length > 0) {
+        availabilityDates = this.state.trainAvailabilty[key];
       }
-    };
+    });
 
-    onSourceStationChange = (e) => {
-      this.setState({
-        sourceStation:e.currentTarget.value
-      })
-    }
-
-    onDestinationStationChange = (e) => {
-      this.setState({
-        destinationStation:e.currentTarget.value
-      })
-    }
-
-    onDateChange = (e) => {
-      this.setState({
-        date:e.currentTarget.value
-      })
-    }
-
-    submit = (e) => {
-      console.log(this.state);
-      fetch(`//api.railwayapi.com/v2/between/source/${this.state.sourceStation}/dest/${this.state.destinationStation}/date/${this.state.date}/apikey/cq5cp33gv8/`)
-      .then(
-        (a)=>a.json()
-      ).then( result  => {
-        console.log(result);
-        this.setState({
-          trainData: result.trains
-        });
-      });
-    }
-
-    tdOnNameclick = (train) => {
-      this.setState({
-        selectedTrain: train
-      });
-      console.log(train)
-      fetch(`https://api.railwayapi.com/v2/check-seat/train/${train.number}/source/${train.from_station.code}/dest/${train.to_station.code}/date/${train.date}/pref/<class code>/quota/GN/apikey/cq5cp33gv8  /`)
-      .then(
-        (a)=>a.json()
-      ).then( result  => {
-        console.log(result);
-        this.setState({
-          trainClass: result.trains
-        });
-      });
-    }
-
-    render() {
-     return (
+    return (
+      <div>
         <form>
-            <input type="text" onChange={this.onSourceStationChange} value={this.state.sourceStation} placeholder="Source Station"></input>
-            <br/>
-            <input type="text" onChange={this.onDestinationStationChange} value={this.state.destinationStation}  placeholder="Destination Station"></input>
-            <br/>
-            <input type="text" onChange={this.onDateChange} value={this.state.date}  placeholder="Date(dd-mm-yyyy)"></input>
-            <br/>
-            <input type="text" placeholder="via"></input>
-            <button onClick={this.submit} type="button">Submit</button>
-            <table>
-                <thead>
-                  <tr>
-                  <th>Train Name</th>
-                  <th>Train No.</th>
-                  <th>ETD</th>
-                  <th>ETA</th>
-                  <th>Travel Time</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    this.state.trainData.map(train => {
-                      return <tr>
-                        <td onClick={() => this.tdOnNameclick(train)}>{train.name}</td>
-                        <td>{train.number}</td>
-                        <td>{train.src_departure_time}</td>
-                        <td>{train.dest_arrival_time}</td>
-                        <td>{train.travel_time}</td>
-                        </tr>
-                    }
-                  )
-                  }  
-                </tbody> 
-            </table>
-            <table>
-                <thead>
-                <tr> {   
-                    this.state.selectedTrain.classes.map(selTrainClass => {
-                      return (<th key={selTrainClass.code}>{selTrainClass.code}</th>);
-                    })}
-                    </tr>
-                </thead>
-            </table>
-    	</form>
+          <div className="form-row">
+            <div className="col">
+              <input
+                type="text"
+                onChange={this.onSourceStationChange}
+                value={this.state.sourceStation}
+                placeholder="Source Station"
+              />
+            </div>
+            <div className="col">
+              <input
+                type="text"
+                onChange={this.onDestinationStationChange}
+                value={this.state.destinationStation}
+                placeholder="Destination Station"
+              />
+            </div>
+            <div className="col">
+              <input
+                type="text"
+                onChange={this.onDateChange}
+                value={this.state.date}
+                placeholder="Date(dd-mm-yyyy)"
+              />
+            </div>
+            <div className="col">
+              <button
+                className="btn btn-primary"
+                onClick={this.submit}
+                type="button"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
+
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Train Name</th>
+              <th>Train No.</th>
+              <th>ETD</th>
+              <th>ETA</th>
+              <th>Travel Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.trainData.map(train => {
+              return (
+                <tr
+                  key={train.number}
+                  className={
+                    train.number === this.state.selectedTrain.number
+                      ? "active"
+                      : ""
+                  }
+                >
+                  <td
+                    className="train-name"
+                    onClick={() => this.selectTrain(train)}
+                  >
+                    {train.name}
+                  </td>
+                  <td>{train.number}</td>
+                  <td>{train.src_departure_time}</td>
+                  <td>{train.dest_arrival_time}</td>
+                  <td>{train.travel_time}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Class</th>
+              {availabilityDates.map(x => <th>{x.date}</th>)}
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(this.state.trainAvailabilty).map(key => {
+              const data = this.state.trainAvailabilty[key];
+              return (
+                <tr key={key}>
+                  {data.length > 0 && <td>{key}</td>}
+                  {data.map(x => <td>{x.status}</td>)}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
